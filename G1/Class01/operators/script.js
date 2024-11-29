0cc580db042497eb0704e19495d62bff77a23f35
trajanstevkovski <trajanstevkovski@gmail.com>
2024-11-29 19:53:37 +0100

    Class01 part-02

---
+++ G1/Class01/operators/script.js
@@ -0,0 +1,43 @@
+let num = 10;
+let num2 = 5;
+
+let sum = num + num2; // 10 + 5 = 15
+console.log(sum);
+
+let substract = num - num2; // 10 - 5 = 5
+console.log("substract");
+console.log(substract);
+
+let multiply = num * num2; // 10 * 5 = 50;
+console.log("Multiply");
+console.log(multiply);
+
+let devide = num / 2; // 10 / 2 = 5;
+console.log("divide");
+console.log(devide);
+
+let modulus = num % 3; // 10 % 3 = 1 // 11 % 3 = 2 // 12 % 3 = 0
+console.log("Modul");
+console.log(modulus);
+
+let num3 = 1;
+num3++;
+console.log("increment");
+console.log(num3); // 2
+
+num3--;
+console.log("decrement");
+console.log(num3); // 1
+
+console.log(5 < 10); // true
+console.log(10 <= 5); // false
+console.log(10 >= 5); // true
+console.log(5 != 5); // false
+
+
+
+
+let a = 5;
+
+a = a + 5;
+a += 5;
---
+++ G1/Class01/variables/script.js
@@ -0,0 +1,45 @@
+let number; // in this case number is undefined
+number = 5;
+
+// console.log(number);
+// console.log(5);
+
+number = 10;
+number = 11;
+
+// console.log(number);
+// console.log(typeof(number));
+
+let firstName = "Trajan"; // '';
+// console.log(typeof(firstName));
+// console.log("The value of firstName is: ")
+// console.log(firstName);
+
+let isValid = true;
+// console.log(typeof(isValid));
+// console.log(isValid);
+
+// isValid = "15";
+// console.log(typeof(isValid));
+// console.log(isValid);
+
+let testVarible;
+// console.log(typeof(testVarible));
+// console.log(testVarible);
+
+let nullValue = null;
+// console.log(typeof(nullValue));
+// console.log(nullValue);
+
+console.log(null === undefined);
+console.log(null == undefined);
+console.log(null === null);
+
+
+// single line comment
+/*  
+    multi line comment
+*/
+
+let newLine = "This is the first line. \nThis goes in a new line";
+console.log(newLine);
