1f2d315c2c4bc54d03d97fa0273be03eaad5599d
trajanstevkovski <trajanstevkovski@gmail.com>
2024-12-02 18:33:08 +0100

    Class02 part-01

---
+++ G1/Class02/strings/script.js
@@ -0,0 +1,35 @@
+let message = "Hello from Qinshift Academy";
+let hello = 'Hello from Qinshift Academy';
+
+console.log(message);
+
+let name = "Filip";
+let helloMessage = "Hello " + name + " from Qinshift Academy";
+
+console.log(helloMessage);
+
+
+// console.log("5" + 5);
+// console.log(5 + "5");
+// console.log(5 - "5");
+
+"www.google.com?id=3&asd=asdghasjdkgas7834&das2=123213213"
+
+let provider = "www.google.com";
+
+let url = provider + "?" + "id=1213" + "&" + "asd=sadhasjd" + "&" + "id2=12312"; 
+
+let id = "id=1234";
+
+let intepoletadeUrl = `${provider}?${id}&id2=asdasd`; 
+
+console.log("First url");
+console.log(url);
+
+console.log("second url with interpolation");
+console.log(intepoletadeUrl);
+
+let quote = "It's really nice to be a programmer";
+let secondQuote = "It's realy \"nice\" to be a programmer";
+console.log(secondQuote);
+
