cba9f3726d56273e660fef11f90509330864f1e1
tijanastojanovska <stojanovska_tijana@outlook.com>
2024-12-03 20:43:35 +0100

    Class 01

---
+++ G6/Class 01/code/feetToMeters.js
@@ -0,0 +1,13 @@
+let feet = 15;
+let conversionRate = 0.3048;
+
+let resultInMeters = feet * conversionRate;
+
+console.log("15 feet in meters:");
+console.log(resultInMeters);
+
+let resultInFeet = resultInMeters / conversionRate;
+
+
+console.log("4.572 meters in feet:");
+console.log(resultInFeet);
