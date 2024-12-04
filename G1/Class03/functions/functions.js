3900a8ff22e9c85277a69c95245dc2efde9f0a1d
trajanstevkovski <trajanstevkovski@gmail.com>
2024-12-04 20:48:04 +0100

    Class03 part-03

--- G1/Class03/functions/functions.js
+++ G1/Class03/functions/functions.js
@@ -54,3 +54,92 @@ function divide(num, num1) {
     //     return -1;
     // }
 }
+
+
+
+
+
+function calculateLoan(amount, months, interest, name) {
+    console.log(amount);
+    console.log(months);
+    console.log(interest);
+    console.log(name);
+}
+
+// calculateLoan();
+// calculateLoan(1, 12);
+// calculateLoan(1, 12, 10, "Trajan", "Filip", 14);
+
+
+function calculator(num, num1, operation = "+", test = 3) {
+    switch(operation) {
+        case "+":
+            return num + num1;
+        case "-":
+            return num - num1;
+        default:
+            return null;
+    }
+}
+
+// console.log(calculator(1, 2));
+// console.log(calculator(12, 2, "-"));
+
+
+// create a function that parses an input from screen to number
+// function should have one argument message that needs to be shown
+// as message on input
+// function should return the parsed number
+// try habndling the bad scenario and if not valid input is entered 
+// return NaN
+
+function parseInputToInteger(message) {
+    let input = prompt(message);
+    let parsedInput = parseInt(input);
+    
+    if (typeof(parsedInput) === 'number' && !Number.isNaN(parsedInput)) {
+        return parsedInput;
+    } else {
+        return NaN;
+    }
+}
+
+let input1 = parseInputToInteger("Please enter number: ");
+let input2 = parseInputToInteger("Vnesete Broj: ");
+let input3 = parseInputToInteger("Vnesete Ime: ");
+console.log(input1);
+console.log(input2);
+console.log(input3);
+
+
+function substract(num, num1) {
+    return num - num1;
+}
+
+function multiply(num, num1) {
+    let result = num * num1;
+    return result;
+}
+
+function advCalculator(num, num1, operation) {
+    if(operation === "+") {
+        let suma = sum(num, num1);
+        return suma;    
+    } else if (operation === "-") {
+        let subs = substract(num, num1);
+        return subs;
+    } else if (operation === "*") {
+        let mlp = multiply(num, num1);
+        return mlp;
+    } else if (operation === "/") {
+        let div = divide(num1, num);
+        return div;
+    } else {
+        return null;
+    }
+}
+
+// advCalculator(5, 10, "+");
+// advCalculator(5, 10, "-");
+// advCalculator(5, 10, "*");
+// advCalculator(5, 10, "/");
