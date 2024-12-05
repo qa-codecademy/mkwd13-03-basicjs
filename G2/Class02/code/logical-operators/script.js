7edfd33e2a835809c958047e33ef06aef3125cb7
Ilija Mitev <94140253+ilijamitev@users.noreply.github.com>
2024-12-05 21:10:52 +0100

    Comments added

--- G2/Class02/code/logical-operators/script.js
+++ G2/Class02/code/logical-operators/script.js
@@ -1,12 +1,14 @@
 console.log("=================== LOGICAL OPERATORS ===================");
+// Used with Boolean values or expressions ***
+// Purpose: to control the flow of code execution
 
-
+// OPERAND(30) OPERATOR(>) OPERAND(50) *
 console.log(30 > 50); // false
 console.log(30 < 50); // true
 
-
 console.log();
 console.log("========== Logical AND && ==========");
+// && returns true if both operands are true, otherwise false
 console.log(true && true);
 console.log(true && false);
 
@@ -22,6 +24,7 @@ console.log(expression); // false
 
 console.log();
 console.log("========== Logical OR || ==========");
+// || returns true if at least one of the operands is true, otherwise false
 console.log(true || false); // true
 console.log(false || true); // true
 console.log(false || false); // false
@@ -42,6 +45,8 @@ console.log(5 > 0 && (5 + 6 < 10) || 10 > 5);
 
 console.log();
 console.log("========== Logical NOT ! ==========");
+// ! is an operator that negates the value of its operand
+
 console.log(!true); // false
 console.log(!false); // true
 
@@ -56,22 +61,46 @@ console.log(!(numOne > numTwo) && "Bob" == "Bob"); // true
 // 2) Bob == Bob => true
 // 3) true && true => true
 
+
 console.log();
 console.log("=================== EQUALITIES ===================");
 
-// console.log(3 = "3");
+// = assignment operator 
+// == equality => evaluates only value*
+// === strict equality => evaluates value* and type* => USED IN MOST CASES
+
+// console.log(3 = "3"); // error
 console.log(3 == "3"); // true
-console.log(3 === "3"); // false
+console.log(3 === "3"); // false => even though the values are same, the type of 3 (number) is not the same as the type of "3" (string)
 
 
 console.log();
 console.log("=================== TRUTHY/FALSY VALUES ===================");
+// => how JavaScript interprets single values in a boolean context
+/* 
+    ***** FALSY VALUES *****
+    - false
+    - 0, -0
+    - "" (empty string)
+    - null
+    - undefined
+    - NaN (invalid number)
+
+    ***** TRUTHY VALUES *****
+    - true
+    - "hello", "0", " "
+    - 25
+    - [ ], [ 1, "2", 3 ] (arrays)
+    - { }, { a: 42 } (objects)
+    - ...
+*/
 
 let numberOne = -1000;
 let numberTwo = 0;
 
+// ways to check if a single value is truthy or falsy
 console.log(Boolean(numberOne)); // true
-console.log(!!numberOne); // true
+// console.log(!!numberOne); // true (fancier syntax with !! instead of Boolean()...)
 console.log(Boolean(numberTwo)); // false
 
 console.log(Boolean("")); // false
@@ -80,6 +109,4 @@ console.log(Boolean(" ")); // true
 console.log(Boolean(null)); // false
 console.log(Boolean(!null)); // true
 
-
-
-
+// Understanding truthy and falsy values is crucial when writing conditional statements *
