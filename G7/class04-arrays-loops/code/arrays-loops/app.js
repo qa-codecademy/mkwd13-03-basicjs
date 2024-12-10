57204f94041c502fb79b0cec120b45587d14674d
borisovski-borche <borisovski.borce@gmail.com>
2024-12-10 18:32:31 +0100

    added code until break one

---
+++ G7/class04-arrays-loops/code/arrays-loops/app.js
@@ -0,0 +1,34 @@
+console.log("This is the start of something great");
+
+//Creating an array
+let fruits = ["apples", "oranges", "pears", "lemons", "mangoes"];
+
+let lemonsIndex = 3;
+//Accessing elements in array
+console.log(fruits[2]);
+console.log(fruits[lemonsIndex]);
+
+let apples = fruits[0];
+
+//Changing values of array elements
+fruits[2] = "cherries";
+
+console.log(fruits[2]);
+
+let students = [];
+
+students[0] = "John Doe";
+students[1] = "Jane Doe";
+
+console.log(students);
+
+//Reading the length of an array
+console.log(fruits);
+console.log(fruits.length);
+console.log(students.length);
+
+//Accessing the last element of an array
+
+let lastElement = fruits[fruits.length - 1];
+
+console.log(lastElement);
