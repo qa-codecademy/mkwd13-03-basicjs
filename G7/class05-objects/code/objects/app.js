3926e33c4fc219ef206275735edee2f55dd57381
borisovski-borche <borisovski.borce@gmail.com>
2024-12-17 19:43:42 +0100

    added class05 until break 2

--- G7/class05-objects/code/objects/app.js
+++ G7/class05-objects/code/objects/app.js
@@ -1,6 +1,6 @@
 console.log("Objects is working");
 
-let studentTwo = {
+let student = {
   //Properties
   firstName: "John",
   lastName: "Doe",
@@ -22,23 +22,103 @@ let studentTwo = {
   },
 };
 
-studentTwo.studyHarder();
-studentTwo.studyHarder();
-studentTwo.studyHarder();
-console.log(studentTwo.knowledge);
+student.studyHarder();
+student.studyHarder();
+student.studyHarder();
+console.log(student.knowledge);
 
 //Accessing properties using . (dot) notation
-console.log(studentTwo.lastName);
-console.log(studentTwo.firstName);
-studentTwo.study();
-studentTwo.printFullName();
+console.log(student.lastName);
+console.log(student.firstName);
+student.study();
+student.printFullName();
 
 //Accessing properties using [] (bracket) notation
-console.log(studentTwo["knowledge"]);
-console.log(studentTwo["age"]);
+console.log(student["knowledge"]);
+console.log(student["age"]);
 //Avopid doing this, can make code hard to work with and read
-console.log(studentTwo["studyHarder"]());
+student["studyHarder"]();
 
 //Updating object properties
+student.firstName = "Bob";
+student.lastName = "Bobski";
+student["knowledge"] = 43;
+
 //Adding properties and methods
+student.country = "USA";
+student.city = "Washington";
+
+student.dontStudy = function () {
+  console.log("student is not studying");
+  this.knowledge -= 10;
+};
+
+student.dontStudy();
+student.dontStudy();
+
 //Deleting properties and methods
+
+// delete student.country;
+// delete student.city;
+//It is a better idea to make the properties emtpy rather than deleting them
+student.country = "";
+student.city = "";
+
+console.log(student);
+
+/*
+Write a JavaScript program to delete the lecture property from the following object. Add a property age to the object. Add a method getFullName that returns the full name ( First Name and Last name )
+*/
+
+let trainer = {
+  name: "Stefan",
+  lastName: "Stefanovski",
+  academy: "SEDC",
+  lecture: "Objects",
+};
+
+// delete trainer.lecture
+trainer.lecture = null;
+
+trainer.age = 54;
+
+trainer.getFullName = function () {
+  return `${this.name} ${this.lastName}`;
+};
+
+console.log(trainer);
+console.log(trainer.getFullName());
+
+function createPersonObject(name, yearOfBirth, job) {
+  let person = {
+    name: name,
+    yearOfBirth: yearOfBirth,
+    job: job,
+  };
+
+  return person;
+}
+
+// let john = createPersonObject("Jonh Doe", 1980, "Truck Driver");
+// let jane = createPersonObject("Jane Doe", 1993, "Nurse");
+
+// console.log(john, jane);
+
+//Constructor notation
+function Person(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+
+  this.calculateAge = function () {
+    let currentYear = new Date().getFullYear();
+
+    console.log(`${this.name} is ${currentYear - this.yearOfBirth} years old`);
+  };
+}
+
+let john = new Person("John Doe", 1980, "Truck Driver");
+let jane = new Person("Jane Doe", 1993, "Nurse");
+let bob = new Person("Bob Bobsky", 1979, "Bartender");
+
+console.log(john, jane, bob);
