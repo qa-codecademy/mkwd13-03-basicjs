d9fa3dfe8a8503596e99ba2d20511cdbc59e8ab6
anetastankovska <anetastankovskaane@gmail.com>
2024-12-18 18:45:31 +0100

    add part 1 code

---
+++ G3/Class07/code/accessing-dom/script.js
@@ -0,0 +1,5 @@
+console.log(document); // logs the entire document (the html)
+
+// we can access everything we keep in the document (html via the document object)
+console.log(document.head);
+console.log(document.URL);
