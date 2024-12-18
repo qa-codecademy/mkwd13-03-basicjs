44565f96c844550c9ceb2483e67e6bb9e287261e
anetastankovska <anetastankovskaane@gmail.com>
2024-12-18 13:41:57 +0100

    Add changes to presentations and add literature

--- G3/Class03/presentation/assets/plugin/highlight/highlight.js
+++ G3/Class03/presentation/assets/plugin/highlight/highlight.js
@@ -1,10 +1,10 @@
 // START CUSTOM REVEAL.JS INTEGRATION
 (function() {
 	if( typeof window.addEventListener === 'function' ) {
-		var hljs_nodes = document.querySelectorAll( 'pre code' );
+		let hljs_nodes = document.querySelectorAll( 'pre code' );
 
 		for( var i = 0, len = hljs_nodes.length; i < len; i++ ) {
-			var element = hljs_nodes[i];
+			let element = hljs_nodes[i];
 
 			// trim whitespace if data-trim attribute is present
 			if( element.hasAttribute( 'data-trim' ) && typeof element.innerHTML.trim === 'function' ) {
