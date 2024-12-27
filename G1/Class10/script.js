3a8c875f7db49fc7e22909a8f7d715d17fcfd08b
trajanstevkovski <trajanstevkovski@gmail.com>
2024-12-27 18:45:28 +0100

    Class10 part-02

--- G1/Class10/script.js
+++ G1/Class10/script.js
@@ -1,6 +1,32 @@
 $(document).ready(function() {
 
-    //let el = document.getElementById('someId') // vanila js
-    let el = $('#someId')
+    let el = document.getElementById('mainTitle') // vanila js
+    let elJquery = $('#mainTitle');
+    // console.log(el);
+    // console.log(elJquery);
+    let allElemenets = $('*');
+    // console.log(allElemenets);
 
+    let elByClassName = $('.innerWrapper');
+    // console.log(elByClassName);
+    let eleByTagName = $('p');
+    // console.log(eleByTagName);
+
+    let eleByMultipleSelectors = $('.innerWrapper, .wrapper, #mainTitle');
+    // console.log(eleByMultipleSelectors);
+
+    // document.getElementsByTagName('p')[0];
+    let firstParagraph = $('p:first');
+    let lastParagraph = $('p:last');
+    // console.log(firstParagraph);
+    // console.log(lastParagraph);
+
+    let firstChildren = $('h3:first-child'); // get all h3 elems, which are first child of their parent
+    let lastChildren = $('h3:last-child'); // get all p elems, which are last child of their parent
+    // console.log(firstChildren);
+    let nthChild = $('p:nth-child(2)');  // get all p elems, which are second child of their parent
+
+    // selecting element using attributes
+    let btn = $('button[name="test"]');
+    // console.log(btn);
 });
